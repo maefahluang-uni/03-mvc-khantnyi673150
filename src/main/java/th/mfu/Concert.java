ee9760e7acdd9a7be167348670a982acb7964708
Khant Nyi Nyi Aung <6731503059@lamduan.mfu.ac.th>
2025-08-25 09:26:20 +0000

    concert_fix

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,15 +5,66 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String performer;
+    private String title;
+    private String description;
+    private Date date;
+    
 
     public Concert() {
     }
+
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description= description;
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
  
     //TODO: add getters and setters for all attributes
+
+    
     // You can use Source action/generate getter setter function
 
 }
